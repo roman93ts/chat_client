90603f511da178b9a80339913e0d6eb90910c352
roman93ts <netsky93@gmail.com>
2019-01-05 01:54:23 +0300

    add video-chat icon

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,12 +2,16 @@ import React from 'react';
 import RoomList from './RoomList';
 import RoomUsers from './RoomUsers';
 import './components.css';
+import imgVideo from './img/video-chat.png';
 
 const Navigation = ({onRoomsList,onUsersList,currentRoom}) => {
 	return (
 		<nav className='nav'>
 			<RoomList onRoomsList = {onRoomsList}/>
-			<div className='roomName'>{currentRoom}</div>
+			<div className = "nameAndVideo">
+				<div className='roomName'>{currentRoom}</div>
+				<button className = 'btnVideo'><img src={imgVideo} alt="video-chat"/></button>
+			</div>
 			<RoomUsers onUsersList = {onUsersList}/>	
 		</nav>
 	);
